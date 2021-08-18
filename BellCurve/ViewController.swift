39e1e66d45fe8fbf37097e9c07379b47eac7a6ac
Greg <greg@passivelogic.com>
2021-08-18 11:28:01 -0600

    Initial Commit

---
+++ BellCurve/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BellCurve
+//
+//  Created by Greg Hughes on 8/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
